0780555d760d503299da0775a40bc4d865c5dbbd
andyvega <andyvega@upeu.edu.pe>
2022-07-20 11:11:41 -0500

    rxjs en services

---
+++ src/app/api.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, mergeMap, of, zip } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  SW_API = 'https://swapi.dev/api';
+
+  constructor(private http: HttpClient) {}
+
+  getData() {
+    return this.http.get(`${this.SW_API}/people/2`).pipe(
+      map((response: any) => ({
+        nombre: response.name,
+        altura: `${response.height} cm`,
+        ojos: (response.eye_color as String).toUpperCase(),
+      }))
+    );
+  }
+
+  getAllData() {
+    return this.http.get(`${this.SW_API}/people/2`).pipe(
+      mergeMap((response: any) =>
+        zip(of(response), this.http.get(response.species[0]))
+      ),
+      map(([personaje, especie]: any[]) => ({
+        nombre: personaje.name,
+        altura: `${personaje.height} cm`,
+        ojos: (personaje.eye_color as String).toUpperCase(),
+        especie: especie.name,
+      }))
+    );
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs-observables';
+  personaje = {
+    nombre: '',
+    altura: '',
+    ojos: ''
+  };
+
+  personajeMerge = {
+    nombre: '',
+    altura: '',
+    ojos: '',
+    especie: ''
+  }
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(){
+    /* this.apiService.getData().subscribe((response: any) => {
+      this.personaje = response || {};
+      console.log('personaje -->', this.personaje);
+    }); */
+
+    this.apiService.getAllData().subscribe((response: any) => {
+      this.personajeMerge = response;
+      console.log('personaje merge-->', response);
+    });
+    
+  }
+
 }
